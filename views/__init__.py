fcc24eb7f136c47d023220ffff10fad94d60f6db
Michaldp <81565882+Michaldp@users.noreply.github.com>
2025-01-21 13:12:55 +0200

    Delete views directory

--- views/__init__.py
+++
@@ -1,9 +0,0 @@
-from .pillar_1 import main as pillar_1
-from .pillar_2 import main as pillar_2
-from .pillar_3 import main as pillar_3
-from .pillar_4 import main as pillar_4
-from .pillar_5 import main as pillar_5
-from .methodology import main as methodology
-
-# Original: imports the main function in the gallery.py for the call in the pages apps.gallery selection.
-from .gallery import main as gallery
